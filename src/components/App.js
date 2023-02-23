a786f3b2eef09fc77861e85e4631d47d382f9f46
CarlaSlattery <cslatterydev@gmail.com>
2023-02-23 11:52:46 +0000

    fixed breaking changes ahead of creation of new router branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import "../styles/app.css";
 import "../styles/search.css";
+
 import React, { useState } from "react";
+
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import logo from "../assets/logo.png";
@@ -12,7 +14,9 @@ const App = () => {
   return (
     <div className="app">
       <img className="nasa-logo" src={logo} alt="nasaLogo" />
+
       <Search setSearchResults={setSearchResults} setLoading={setLoading} />
+
       <SearchResults results={searchResults} loading={loading} />
     </div>
   );
